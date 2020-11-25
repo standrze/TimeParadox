9f3aaa7ed9f751a7102802a6c0eb5c4e7c5db314
Stephen <sandrzejewski@berkeley.edu>
2020-11-24 21:18:30 -0500

    Initial Commit

--- main.go
+++ main.go
@@ -1,27 +1,9 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-// Package main implements a server for Greeter service.
 package main
 
 import (
 	pb "TimeParadox/paradox"
 	"context"
+	"fmt"
 	"github.com/google/martian"
 	"github.com/google/martian/fifo"
 	"google.golang.org/grpc"
@@ -31,55 +13,46 @@ import (
 	"sync"
 )
 
-const (
-	port        = ":50051"
-	defaultPort = "8080"
-)
-
 type Server struct {
 	pb.UnimplementedParadoxServer
-	mu           sync.Mutex
 	notification chan string
 	latest       string
 }
 
-func (m *Server) ModifyRequest(req *http.Request) error {
-	m.latest = req.RequestURI
+func (s *Server) ModifyRequest(req *http.Request) error {
 	return nil
 }
 
-func (m *Server) HelloWorld(ctx context.Context, in *pb.Request) (*pb.Reply, error) {
-	//s.mu.Lock()
-	//st := <-s.notification
-	//log.Printf("Received: %v", in.GetName())
-	//s.mu.Unlock()
-	return &pb.Reply{Message: "Hello " + m.latest}, nil
+func (s *Server) HelloWorld(ctx context.Context, in *pb.Request) (*pb.Reply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.Reply{Message: "Hello " + s.latest}, nil
 }
 
-func (m *Server) StartProxy() {
+func (s *Server) StartProxy() {
 	proxy := martian.NewProxy()
-	listener, err := net.Listen("tcp", defaultPort)
+
+	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to listen proxy: %v", err)
 	}
 
 	top := fifo.NewGroup()
 
-	top.AddRequestModifier(m)
+	top.AddRequestModifier(s)
 	proxy.SetRequestModifier(top)
 
 	go proxy.Serve(listener)
 }
 
-func (m *Server) StartRPC() {
+func (s *Server) StartRPC() {
 	server := grpc.NewServer()
 
-	lis, err := net.Listen("tcp", port)
+	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to listen : %v", err)
 	}
 
-	pb.RegisterParadoxServer(server, m)
+	pb.RegisterParadoxServer(server, s)
 
 	if err := server.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
@@ -99,5 +72,4 @@ func main() {
 
 	mod.StartProxy()
 	mod.StartRPC()
-
 }
